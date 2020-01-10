b9de0610f5b29f824b005f3b6b31c9bb0bcec837
Akshay Venkatesh <akvenkatesh@nvidia.com>
2020-01-10 15:42:55 -0800

    TEST/EXAMPLES: Add NVIDIA license information

--- test/examples/ucp_hello_world_one_sided.c
+++ test/examples/ucp_hello_world_one_sided.c
@@ -1,6 +1,7 @@
 /**
 * Copyright (C) Mellanox Technologies Ltd. 2001-2016.  ALL RIGHTS RESERVED.
 * Copyright (C) Advanced Micro Devices, Inc. 2018. ALL RIGHTS RESERVED.
+* Copyright (C) NVIDIA Corporation. 2020. ALL RIGHTS RESERVED.
 *
 * See file LICENSE for terms.
 */
